f4369b4e0d06f7a10d318f16b9abcd5a1d025c28
Meraj <mdmaraj1500@gmail.com>
2023-07-30 22:18:41 +0600

    my profile prompt edit and delete func created

--- components/Form.jsx
+++ components/Form.jsx
@@ -38,7 +38,7 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
           <input
             value={post.tag}
             onChange={(e) => setPost({ ...post, tag: e.target.value })}
-            placeholder="#tag"
+            placeholder="tag"
             required
             className="form_input"
           />
